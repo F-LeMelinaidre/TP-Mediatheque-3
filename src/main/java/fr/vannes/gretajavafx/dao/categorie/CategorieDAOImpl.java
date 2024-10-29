f9f2411debc8e5e8aaa446e164ed1c35eef1f4e8
major <major.max.englishbull@gmail.com>
2024-10-29 19:07:26 +0100

    Afficher media Afficher Emprunt

--- src/main/java/fr/vannes/gretajavafx/dao/categorie/CategorieDAOImpl.java
+++ src/main/java/fr/vannes/gretajavafx/dao/categorie/CategorieDAOImpl.java
@@ -2,12 +2,13 @@ package fr.vannes.gretajavafx.dao.categorie;
 
 import fr.vannes.gretajavafx.dao.DAOFactory;
 import fr.vannes.gretajavafx.model.Categorie;
+import fr.vannes.gretajavafx.model.SousCategorie;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import static fr.vannes.gretajavafx.dao.Config.*;
 
 public class CategorieDAOImpl implements CategorieDAO<Categorie> {
 
@@ -23,18 +24,21 @@ public class CategorieDAOImpl implements CategorieDAO<Categorie> {
         }
     }
 
-    public static CategorieDAOImpl get_instance() {
+    public static CategorieDAOImpl get_instance(DAOFactory df) {
         if (_instance == null) {
             try {
-                _df       = DAOFactory.getInstance();
+                _df       = df;
                 _instance = new CategorieDAOImpl(_df);
-
             } catch (SQLException e) {
                 System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+                // Lancer une RuntimeException
+                throw new RuntimeException("Erreur lors de l'initialisation de MediaDAOImpl", e);
             } catch (Exception e) {
                 e.printStackTrace();
+                throw new RuntimeException("Erreur inconnue lors de l'initialisation de MediaDAOImpl", e);
             }
         }
+
         return _instance;
     }
 
@@ -49,7 +53,7 @@ public class CategorieDAOImpl implements CategorieDAO<Categorie> {
     }
 
     @Override
-    public ArrayList findAll() {
+    public ArrayList<Categorie> findAll() {
         return null;
     }
 
@@ -61,5 +65,54 @@ public class CategorieDAOImpl implements CategorieDAO<Categorie> {
     @Override
     public Boolean delete(Categorie categorie) {
         return null;
+    }
+
+    @Override
+    public ArrayList findAll(boolean sousCategories) {
+        ArrayList<Categorie> categorieList = new ArrayList<>();
+        ArrayList<Integer> categorieIds = new ArrayList<>();
+        Categorie categorie = null;
+        ResultSet rs = null;
+
+        String select = "SELECT c.categorie_id as categorieId, c.label as categorieLabel";
+        String from = " FROM categorie as c ";
+
+        if (sousCategories) {
+            select += ", sc.sous_categorie_id as sousCategorieId, sc.label as sousCategorieLabel ";
+            from += " INNER JOIN category_has_subcategory cs ON cs.categorie_id = c.categorie_id ";
+            from += " INNER JOIN sous_categorie as sc ON sc.sous_categorie_id = cs.sous_categorie_id ";
+        }
+
+        String sql = select + from;
+
+        try (PreparedStatement ps = _conn.prepareStatement(sql)) {
+
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int categorieId = rs.getInt("categorieId");
+                String categorieLabel = rs.getString("categorieLabel");
+
+                if(!categorieIds.contains(categorieId)) {
+                    categorie = new Categorie(categorieId, categorieLabel);
+                    categorieList.add(categorie);
+                }
+
+                if(!sousCategories) {
+                    int sousCategorieId = rs.getInt("sousCategorieId");
+                    String sousCategorieLabel = rs.getString("sousCategorieLabel");
+                    SousCategorie sousCategorie = new SousCategorie(sousCategorieId, sousCategorieLabel);
+                    categorie.addSousCategorie(sousCategorie);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            _df.closeResultSet(rs);
+            _df.closeConnection();
+        }
+
+        return categorieList;
     }
 }
